6100d3c4470b7ec70b9afe4976938bfed6fb9be7
Bao Nguyen <thanhbao0408@gmail.com>
2022-04-27 11:05:26 +0700

    Add Polly for Shop Aggregrator HttpClient

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,27 +1,61 @@
 using Common.Logging;
 using Microsoft.OpenApi.Models;
+using Polly;
+using Polly.Extensions.Http;
 using Serilog;
 using Shopping.Aggregator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var getRetryPolicy = () =>
+{
+    // In this case will wait for
+    //  2 ^ 1 = 2 seconds then
+    //  2 ^ 2 = 4 seconds then
+    //  2 ^ 3 = 8 seconds then
+    //  2 ^ 4 = 16 seconds then
+    //  2 ^ 5 = 32 seconds
+
+    return HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .WaitAndRetryAsync(
+        retryCount: 5,
+        sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+        onRetry: (exception, retryCount, context) =>
+        {
+            Log.Error($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+        });
+};
+
+var getCircuitBreakerPolicy = () =>
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .CircuitBreakerAsync(
+            handledEventsAllowedBeforeBreaking: 5,
+            durationOfBreak: TimeSpan.FromSeconds(30)
+        );
+};
+
+builder.Services.AddTransient<LoggingDelegatingHandler>();
+
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]));
-//.AddHttpMessageHandler<LoggingDelegatingHandler>()
-//.AddPolicyHandler(GetRetryPolicy())
-//.AddPolicyHandler(GetCircuitBreakerPolicy());
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]))
+            .AddHttpMessageHandler<LoggingDelegatingHandler>()
+            .AddPolicyHandler(getRetryPolicy())
+            .AddPolicyHandler(getCircuitBreakerPolicy());
 
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]));
-//.AddHttpMessageHandler<LoggingDelegatingHandler>()
-//.AddPolicyHandler(GetRetryPolicy())
-//.AddPolicyHandler(GetCircuitBreakerPolicy());
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]))
+.AddHttpMessageHandler<LoggingDelegatingHandler>()
+.AddPolicyHandler(getRetryPolicy())
+.AddPolicyHandler(getCircuitBreakerPolicy());
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]));
-//.AddHttpMessageHandler<LoggingDelegatingHandler>()
-//.AddPolicyHandler(GetRetryPolicy())
-//.AddPolicyHandler(GetCircuitBreakerPolicy());
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]))
+.AddHttpMessageHandler<LoggingDelegatingHandler>()
+.AddPolicyHandler(getRetryPolicy())
+.AddPolicyHandler(getCircuitBreakerPolicy());
 
 
 builder.Services.AddControllers();
@@ -48,3 +82,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
